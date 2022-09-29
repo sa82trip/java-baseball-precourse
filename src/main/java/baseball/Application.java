ac6b0a16a20901cc1398403e191f8a0f914834a5
Joonsung Gil <sa82trip@gmail.com>
2022-09-30 01:04:36 +0900

    feat(Baseball): add feature to finish game
    when user gets three strikes then game should be finished
    
    BREAKING CHANGE: Before:
    
    Even though user gets three strikes game just keeps going
    
    After:
    
    After user gets three strikes game is done

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,16 +4,28 @@ import baseball.game.Baseball;
 import baseball.player.Player;
 import baseball.umpire.Umpire;
 import baseball.util.Util;
+import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
+    private final static String NEW_GAME = "1";
+    private final static String END_GAME = "2";
+
     public static void main(String[] args) {
         // TODO: 프로그램 구현
         Util util = new Util();
         Player computer = new Player();
-        computer.setInputNumber(util.makeRandomNumberInString());
         Player user = new Player();
         Umpire umpire = new Umpire();
-        Baseball baseball = new Baseball(computer, user, umpire);
-        baseball.beginGame();
+        Baseball baseball = new Baseball(computer, user, umpire, util);
+
+        String flg = NEW_GAME;
+        while (flg.equals(NEW_GAME)) {
+            baseball.beginGame();
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.printf("게임을 새로 시작하려면 %s, 종료하려면 %s를 입력하세요%n", NEW_GAME, END_GAME);
+            flg = Console.readLine();
+        }
+        System.out.println("게임 종료");
+//        System.exit(0);
     }
 }
--- src/main/java/baseball/game/Baseball.java
+++ src/main/java/baseball/game/Baseball.java
@@ -2,6 +2,7 @@ package baseball.game;
 
 import baseball.player.Player;
 import baseball.umpire.Umpire;
+import baseball.util.Util;
 import camp.nextstep.edu.missionutils.Console;
 
 public class Baseball {
@@ -9,29 +10,48 @@ public class Baseball {
     Player user;
     Umpire umpire;
 
-    public Baseball(Player computer, Player user, Umpire umpire) {
+    Util util;
+
+    public Baseball(Player computer, Player user, Umpire umpire, Util util) {
         this.computer = computer;
         this.user = user;
         this.umpire = umpire;
+        this.util = util;
     }
 
     public void beginGame() {
 
         boolean finish = false;
 
-        //TODO: add determiner to finish baseball game
+        computer.setInputNumber(util.makeRandomNumberInString());
+
         while (!finish) {
             System.out.print("숫자를 입력해주세요:");
-            try {
-                user.setInputNumber(Console.readLine());
-            } catch (IllegalArgumentException iAE) {
-                System.out.println("숫자는 동일한 숫자를 포함해서는 안되며");
-                System.out.println("0을 포함해서도 안됩니다");
-                System.out.print("숫자를 입력해주세요:");
-                user.setInputNumber(Console.readLine());
-            }
-            System.out.println(umpire.judge(computer.getInputNumber(), user.getInputNumber()));
+            user.setInputNumber(Console.readLine());
+            String result = umpire.judge(computer.getInputNumber(), user.getInputNumber());
+            System.out.println(displayResult(result.split(":")));
+            finish = needToFinish(result);
         }
+    }
+
+    private String displayResult(String[] result) {
+        String ballPart = String.format("%s볼", result[0]);
+        String strikePart = String.format("%s스트라이크", result[1]);
+        if (ballPart.contains("0") && strikePart.contains("0")) {
+            return "낫싱";
+        }
+        if (ballPart.contains("0") && !strikePart.contains("0")) {
+            return strikePart;
+        }
+        if (strikePart.contains("0") && !ballPart.contains("0")) {
+            return ballPart;
+        }
+        return ballPart + " " + strikePart;
+    }
+
+
+    private boolean needToFinish(String judge) {
+        return judge.endsWith("3");
     }
 }
 
