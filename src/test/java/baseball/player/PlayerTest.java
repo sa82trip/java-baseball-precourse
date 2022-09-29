620d0bf1cd049f3a8de7843421391190e8ba21e1
Joonsung Gil <sa82trip@gmail.com>
2022-09-29 21:09:51 +0900

    test(PlayerTest): add test class with three cases
    add test class with cases for ensure that setter and validator work fine
    ( setter와 validator가 제댜로 작동하는지 확인하기 위해 test class를 추가)

---
+++ src/test/java/baseball/player/PlayerTest.java
@@ -0,0 +1,31 @@
+package baseball.player;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+class PlayerTest {
+
+    @Test
+    @DisplayName("number which is not 3-digit number should result to get False")
+    void validateInputFalse() {
+        Player unit = new Player();
+        assertThat(unit.validateInput("1234")).isFalse();
+    }
+
+    @Test
+    @DisplayName("number which is 3-digit number should result to get True")
+    void validateInputSuccess() {
+        Player unit = new Player();
+        assertThat(unit.validateInput("123")).isTrue();
+    }
+
+    @Test
+    @DisplayName("setting number which is not 3-digit to model should result IllegalArgumentException")
+    void setInputNumber() {
+        Player unit = new Player();
+        assertThrows(IllegalArgumentException.class, () -> unit.setInputNumber("1234"));
+    }
+}
