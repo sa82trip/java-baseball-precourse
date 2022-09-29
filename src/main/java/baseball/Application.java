5bbd2c02c78dd60d9e7efaa7052a2270fd7244b1
Joonsung Gil <sa82trip@gmail.com>
2022-09-29 13:18:36 +0900

    fix(Util): fix bug on `makeRandomNumberInString()`
    makeRandomNumberInString() can possibly make number including same numbers in it.
    
    BREAKING CHANGE: Before:
    
    makeRandomNumberInString() can possibly make number including same numbers in it
    
    After:
    
    if randomNumbers contains number which is same with the number is being added to randomNumber then throw IllegalArgumentException

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -7,8 +7,9 @@ import baseball.util.Util;
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        Util util = new Util();
         Player computer = new Player();
-        computer.setThreeDigitNumber(Util.makeRandomNumberInString());
+        computer.setThreeDigitNumber(util.makeRandomNumberInString());
         Player user = new Player();
         Baseball baseball = new Baseball(computer, user);
         baseball.beginGame();
--- src/main/java/baseball/util/Util.java
+++ src/main/java/baseball/util/Util.java
@@ -1,15 +1,43 @@
 package baseball.util;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Util {
-    public static String makeRandomNumberInString() {
-        int limit = 0;
-        StringBuilder randomNumber = new StringBuilder();
-        while (limit < 3) {
-            randomNumber.append(Randoms.pickNumberInRange(1, 9));
-            limit++;
-        }
-        return randomNumber.toString();
+    public String makeRandomNumberInString() {
+        return getStringifiedNumber();
     }
+
+    private String getStringifiedNumber() {
+        StringBuilder result = new StringBuilder();
+        for (Integer number : getRandomNumber(3)) {
+            result.append(number);
+        }
+        return result.toString();
+    }
+
+    private List<Integer> getRandomNumber(int digits) {
+        int limit = 0;
+        List<Integer> randomNumbers = new ArrayList<>();
+        while (limit < digits) {
+            int randomNumber = Randoms.pickNumberInRange(1, 9);
+            try{
+                randomNumbers.add(barf(randomNumbers, randomNumber));
+                limit++;
+            }catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+                getRandomNumber(digits);
+            }
+        }
+        return randomNumbers;
+    }
+
+    private Integer barf(List<Integer> numbers, int number) {
+        if (numbers.contains(number)) {
+            throw new IllegalArgumentException("can not add duplicate number");
+        }
+        return number;
+    }
+
 }
