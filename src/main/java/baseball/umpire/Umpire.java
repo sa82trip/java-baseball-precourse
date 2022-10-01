b754f2ed895a5d0816118126e69a7d37d3af9680
Joonsung Gil <sa82trip@gmail.com>
2022-10-01 11:45:01 +0900

    refactor(Umpire): refactor to better readability
    make less lines method by extracting logic from one method

--- src/main/java/baseball/umpire/Umpire.java
+++ src/main/java/baseball/umpire/Umpire.java
@@ -6,17 +6,24 @@ public class Umpire {
         int strike = 0;
         int ball = 0;
         for (int i = 0; i < numberInComputer.length(); i++) {
-            if (numberInComputer.charAt(i) == numberInUser.charAt(i)) {
-                strike++;
-                continue;
-            }
-            if (numberInComputer.charAt(i) != numberInUser.charAt(i)
-                    && numberInComputer.contains("" + numberInUser.charAt(i))) {
-                ball++;
-            }
+            strike += isAStrike(numberInComputer, numberInUser, i);
+            ball += isABall(numberInComputer, numberInUser, i);
         }
         return String.format("%d:%d", ball, strike);
     }
 
+    protected int isABall(String numberInComputer, String numberInUser, int i) {
+        if (numberInComputer.charAt(i) != numberInUser.charAt(i)
+                && numberInComputer.contains(String.valueOf(numberInUser.charAt(i)))) {
+            return 1;
+        }
+        return 0;
+    }
 
+    protected int isAStrike(String numberInComputer, String numberInUser, int i) {
+        if (numberInComputer.charAt(i) == numberInUser.charAt(i)) {
+            return 1;
+        }
+        return 0;
+    }
 }
